ef6d23e5893920640ed7c65be062416efe8b54c8
WooDoo1337 <slaviiklll@mail.ru>
2025-11-25 22:07:52 +1000

    ... - 95

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.core.paginator import Paginator, EmptyPage
+from django.core.paginator import Paginator, EmptyPage, \
+                                  PageNotAnInteger
+from django.views.generic import ListView
 
 
 def post_list(request):
@@ -9,6 +11,8 @@ def post_list(request):
     page_number = request.GET.get('page', 1)
     try:
         posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
     except EmptyPage:
         posts = paginator.page(paginator.num_pages)
     return render(request,
@@ -28,3 +32,11 @@ def post_detail(request, year, month, day, post):
                   {'post': post})
 
 
+class PostListView(ListView):
+    """
+    Альтернативное представление списка постов
+    """
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
