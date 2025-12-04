320e76651ac695a07a62b9d041bd204ea15a138b
WooDoo1337 <slaviiklll@mail.ru>
2025-12-04 19:34:26 +1000

    ... - 135

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -27,9 +28,11 @@ class Post(models.Model):
     status = models.CharField(max_length=2,
                               choices=Status.choices,
                               default=Status.DRAFT)
+    
 
     objects = models.Manager()
     published = PublishedManager()
+    tags = TaggableManager()
     
     class Meta:
         ordering = ['-publish']
