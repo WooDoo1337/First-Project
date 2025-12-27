6643da7505901235aefe69f3f48b3fcfcdfe541e
WooDoo1337 <slaviiklll@mail.ru>
2025-12-27 19:20:03 +1000

    ... - 248

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_pm$g&$$nw)^un7a!fr&049d9%gvbh4uc&7ns16l)iw6l#l06n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mysite.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'social_django',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
